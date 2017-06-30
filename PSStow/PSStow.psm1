d5d2c8cb1de366dc875558d9435af3af54eb59f1
Hassan, Tahir <th203@leicester.ac.uk>
2017-06-30 09:03:39 +0100

    adding the module and manifest
    further improvements to documentation
    
    Readme.md - changed a lot of text, proof-read by ant.

---
+++ PSStow/PSStow.psm1
@@ -0,0 +1,149 @@
+Set-StrictMode -Version Latest
+$ErrorActionPreference = 'Stop';
+
+Function CreateResult($Success, $Message, $Item, $Store) {
+	return [pscustomobject]@{
+		'Success' = $Success;
+        'Message' = $Message;
+        'Item' = $Item;
+		'Store' = $Store
+	};
+}
+
+Function GetRoot([string]$Path) {
+    return [System.IO.Path]::GetPathRoot($Path);
+}
+
+Function GetAbsolutePath {
+    param([string]$Path)
+    
+    [IO.Path]::GetFullPath([IO.Path]::Combine((Get-Location).ProviderPath, $Path));
+}
+
+Function RemoveRoot([string]$Path) {
+    $root = GetRoot $Path
+    $Path.Replace($root, '');
+}
+
+Function ValidatePathStoreRelationship {
+    param([string]$Path, [string]$Store)
+
+    $Path = GetAbsolutePath $Path;
+    $Store = GetAbsolutePath $Store;
+
+    # for now, then change it to allow different roots.
+    if ((GetRoot $Path) -ne (GetRoot $Store)) {
+        return 'Item Path and Store must in the same root';
+    }
+
+    if ($Path.StartsWith($Store)) {
+        return 'Item cannot be be a sub-directory of Store';
+    }
+
+    if ($Store.StartsWith($Path)) {
+        return 'Item cannot be a parent directory of Store';
+    }
+}
+
+Function GetAbsoluteStorePath {
+    param([string]$Path, $Store) 
+
+    # e.g. given a store C:\Dirs\MyStore and path to safeStore like C:\Users\th203\hello\world, you get
+    # C:\Dirs\MyStore\Users\th203\hello\world
+    return [IO.Path]::Combine((GetAbsolutePath $Store), (RemoveRoot (GetAbsolutePath $Path)));
+}
+
+Function Stow-Item {
+    param([Parameter(Mandatory=$true,Position=0,ValueFromPipeline=$true)][string[]]$Path, [Parameter(Mandatory=$true)][string]$Store)
+    
+    process {
+        foreach ($Item in $Path) {
+            
+            $createResult = { 
+                param($Success, $Message)
+                return CreateResult -Success $Success -Item $Item -Message $Message -Store $Store
+            };
+
+            $validationResult = ValidatePathStoreRelationship -Path $Item -Store $Store;
+
+            if ($validationResult) {
+                return &$createResult $false $validationResult
+            }
+
+            if (-not (Test-Path $Item)) {
+                return &$createResult $null 'Item does not exist'
+            }
+            
+            try {
+                $fullStorePath = GetAbsoluteStorePath $Item $Store;
+
+                if (Test-Path $fullStorePath) {
+                    return &$createResult $false "There is already a folder $fullStorePath"  
+                }
+                
+                # given a fullpath of C:\Dirs\MyStore\Users\th203\hello\world, you get 
+                # C:\Dirs\MyStore\Users\th203\hello\
+                $storeParent = Split-Path $fullStorePath;
+                if (-not (Test-Path $storeParent)) {
+                    # create the parent directory if it does not exist
+                    mkdir $storeParent -ErrorAction Stop | Out-Null
+                }
+                
+                Move-Item $Item $storeParent -ErrorAction Stop
+                return &$createResult $true "Successfully moved $Item to $storeParent"
+                
+            } catch {
+                return &$createResult $false $_.Exception.Message  
+            }
+        }
+    }
+}
+
+Function Unstow-Item {
+    param([Parameter(Mandatory=$true,Position=0,ValueFromPipeline=$true)][string[]]$Path, [Parameter(Mandatory=$true)][string]$Store)
+
+    process {
+        foreach ($Item in $Path) {
+
+            $createResult = { 
+                param($Success, $Message)
+                return CreateResult -Success $Success -Item $Item -Message $Message -Store $Store
+            };
+
+            # given $store C:\_data\Store
+            # and a $Item C:\mydata\myjavadata
+            # $storeLocation will be  C:\_data\Store\mydata\myjavadata
+            $storeLocation = GetAbsoluteStorePath $Item $Store;
+
+            $validationResult = ValidatePathStoreRelationship -Path $Item -Store $Store;
+
+            if ($validationResult) {
+                return &$createResult $false $validationResult;
+            }
+
+            if (-not (Test-Path $storeLocation)) {
+                return &$createResult $null "Store location does not exist"  
+            }
+            
+            if (Test-Path $Item) {
+                return &$createResult $false "Destination exists: $( GetAbsolutePath $Item )"  
+            }
+            
+            try {
+                $parentPath = Split-Path $Item;
+
+                if (-not (Test-Path $parentPath)) {
+                    mkdir $parentPath -ErrorAction Stop | Out-Null
+                }
+
+                Move-Item $storeLocation $parentPath -ErrorAction Stop;
+                return &$createResult $true "Successfully returned to $Item"  
+            } catch {
+                return &$createResult $false $_.Exception.Message  
+            }
+        }
+    }
+}
+
+Export-ModuleMember -Function Stow-Item
+Export-ModuleMember -Function Unstow-Item
---
+++ README.md
@@ -0,0 +1,89 @@
+# PSStow
+
+This PowerShell module provides the ability to stow away an item, and to then unstow that item.  By "stowing", we mean moving the item away for safekeeping, and then to unstow means to restore it to its original location.
+
+In some networked environments, there is a size limit on your `USERPROFILE` directory.  You can keep its size down by stowing directories when you log off and then unstow these directories when you log on.
+
+## Installation
+
+### Install from PowerShellGallery (preferred)
+
+You will need PowerShellGet.  It is included in Windows 10 and [WMF5](http://go.microsoft.com/fwlink/?LinkId=398175). If you are using PowerShell V3 or V4, you will need to install [PowerShellGet](https://www.microsoft.com/en-us/download/details.aspx?id=49186).
+
+After installing PowerShellGet, you can simply run `Install-Module PSStow`.
+
+### Installing from GitHub
+
+You can download the `PSStow` folder from this repository and copy it to one of your modules directory, using this Microsoft guide to [Installing a PowerShell Module][ms].
+
+[ms]: https://msdn.microsoft.com/en-us/library/dd878350(v=vs.85).aspx
+
+## Usage
+
+Both `Stow-Item` and `Unstow-Item` return  a `pscustomobject` with the properties:
+
+* `Success` - can be either `true` or `false` depending on if it succeeded, or `null` if it can't be classified as either.
+* `Item` - the path of the item being stowed or unstowed.
+* `Store` - the (root) path of the store containing the stowed/unstowed item.
+* `Message` - a message to help with diagnosing any issues that arise.
+
+### Stowing a Directory
+
+To stow a directory `C:\Users\<username>\.android` into a store `C:\_store`, use the `Stow-Item` function:
+
+```powershell
+Stow-Item -Path "$env:USERPROFILE\.android" -Store C:\_store
+```
+
+### Unstowing a directory
+
+To unstow the stowed directory, you call `Unstow-Item` passing in the same arguments as you did to `Stow-Item`:
+
+```powershell
+Unstow-Item -Path "$env:USERPROFILE\.android" -Store C:\_store
+```
+
+## Advanced Usage
+
+Typically there are a set of offending User Profile directories that are too large to be sent over the network as part of a roaming profile.  You can define two functions to deal with this situation:
+
+* `stowUserProfileDirs` -  this will move massive directories out of `USERPROFILE` into `C:\_store`.
+* `unstowUserProfileDirs` -  this will move the massive directories back for use.
+
+```powershell
+$global:stowStore =  "C:\_store";
+
+$global:userProfileDirsToStow = @(
+		"$env:LOCALAPPDATA\Google\Chrome\"                # Chrome dir settings
+	, "$env:LOCALAPPDATA\Microsoft\VisualStudio\"      # Visual Studio settings (different versions)
+
+	, "$env:USERPROFILE\.nuget\"                       # nuget cache
+	, "$env:USERPROFILE\.android\"                     # visual studio's android files
+
+	, "$env:LOCALAPPDATA\Microsoft\SQL Server Management Studio\" # SSMS settings (different versions)
+
+	, "$env:LOCALAPPDATA\atom"                         # atom program dir
+
+	, "$env:USERPROFILE\.atom\"                        # atom settings dir
+
+	, "$env:LOCALAPPDATA\ApexSQL"                      # ApexSQL settings dir
+);
+
+Function stowUserProfileDirs {
+	$global:userProfileDirsToStow | Stow-Item -Store $global:stowStore
+}
+
+Function unstowUserProfileDirs {
+	$global:userProfileDirsToStow | Unstow-Item -Store $global:stowStore
+}
+```
+
+You would put the above in your PowerShell profile, and when you log on you call `stowUserProfileDirs` and before you log off, you call `unstowUserProfileDirs`.
+
+The alternative is to have `stowUserProfileDirs` to be called automatically on log on, and `unstowUserProfileDirs` called automatically before log off, which can be done using [Group Policy Editor][gpo].
+
+[gpo]: https://technet.microsoft.com/en-us/library/cc725970(v=ws.11).aspx
+
+## Licence
+
+This project is under the MIT licence.
